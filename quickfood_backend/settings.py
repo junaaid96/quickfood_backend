3e1dd02bad538404ff5bc737ed45b2f41b3dbfac
junaaid96 <info.junaidulislam@gmail.com>
2025-03-21 14:09:46 +0600

    db changed

--- quickfood_backend/settings.py
+++ quickfood_backend/settings.py
@@ -91,17 +91,22 @@ WSGI_APPLICATION = 'quickfood_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"postgresql://{DB_USER}:{DB_PASSWORD}@ep-wild-hall-a10h1gea-pooler.ap-southeast-1.aws.neon.tech/quickfood?sslmode=require",
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
